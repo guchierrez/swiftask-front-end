8f4be8ff87e4e04d71a3d21e49b5aefecc0dffdf
guchierrez <marcelinogs2001@gmail.com>
2023-11-09 13:15:05 -0300

    feat: add toast warning that api is loading

--- src/components/general/AddTaskButton.tsx
+++ src/components/general/AddTaskButton.tsx
@@ -1,12 +1,19 @@
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { useContext } from "react";
 import { MainContext } from "../../providers/MainContext";
+import toast from "react-hot-toast";
 
 export const AddTaskButton = () => {
-  const { addTaskModalRef } = useContext(MainContext);
+  const { addTaskModalRef, mainLoading } = useContext(MainContext);
   return (
     <button
-      onClick={() => addTaskModalRef.current?.showModal()}
+      onClick={() => {
+        if (mainLoading) {
+          toast("Fetching data from API, please wait or refresh the website.");
+        } else {
+          addTaskModalRef.current?.showModal();
+        }
+      }}
       className="mx-auto border-0 rounded-full btn btn-outline w-fit h-fit"
     >
       <AiOutlinePlusCircle size="2em" />
