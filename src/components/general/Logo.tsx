c7c0f897d7219c7b41fb1c5b53de73922e714d3d
Marcelino Gutierrez <marcelinogs2001@gmailcom>
2024-01-20 13:24:48 -0300

    update logo

--- src/components/general/Logo.tsx
+++ src/components/general/Logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => {
   return (
-    <h1 className="text-xs font-black tracking-widest break-words whitespace-normal select-none md:text-md lg:text-lg">
-      MEISTERS
+    <h1 className="text-xs tracking-widest break-words whitespace-normal select-none md:text-md lg:text-lg">
+      Swiftask
     </h1>
   );
 };
